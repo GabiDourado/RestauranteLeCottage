e0c19e7d6b423834679d24236cd6bd9c45bf12ba
GabiDourado <gabi.dourados2xd@gmail.com>
2024-04-02 10:00:12 -0300

    adição do perfil e ajustes na reserva

--- src/PedidoAnterior.js
+++ src/PedidoAnterior.js
@@ -2,18 +2,30 @@ import { Text, View, StyleSheet, Image} from "react-native";
 
 export default function PedidoAnterior({foto, nome, preco, data}){
     return(
-        <View>
-            <Image style={css.teste} source={{uri: foto,}}></Image>
-            <Text>{data}</Text>
-            <Text>{nome}</Text>
-            <Text>{preco}</Text>    
+        <View tyle={css.tudo}>
+            <Image style={css.img} source={{uri: foto,}}></Image>
+            <Text style={css.data}>{data}</Text>
+            <Text style={css.nome}>{nome}</Text>
+            <Text style={css.preco}>{preco}</Text>    
         </View>
     );
 }
 
 const css = StyleSheet.create({
-    teste: {
+    tudo: {
+        display: 'flex'
+    },
+    img: {
         width: 100,
         height: 100
+    },
+    data: {
+        fontSize: 20
+    },
+    nome: {
+
+    },
+    preco: {
+
     }
 });
--- src/Reserva.js
+++ src/Reserva.js
@@ -1,22 +1,32 @@
 import { Text, TouchableOpacity, View, TextInput, StyleSheet } from "react-native";
-import Calendario from "./Calendario";
+import CalendarPicker from "react-native-calendar-picker";
 
 
-export default function Reserva () {
+export default function Reserva ({date}) {
+
     return(
         <View style={css.Todo}>
-            <Text>Faça sua reserva</Text>
-            <Text>Faites votre réservation</Text>
-            <Text>Data</Text>
-            <TextInput keyboardType="numeric" placeholder="dd-mm-aaaa"></TextInput>
-            <Text>Hora</Text>
-            <TextInput keyboardType="numeric" placeholder="hh.mm"></TextInput>
-            <Text>Pessoas</Text>
-            <TextInput keyboardType="numeric" placeholder="000"></TextInput>
-            <TouchableOpacity>
-                <Text>Agendar</Text>
-            </TouchableOpacity>
-           
+            <View style={css.titulo}>
+                <View style={css.linha}>
+                    <Text style={css.tituloPT}>Faça sua reserva</Text>
+                </View>
+                <Text style={css.tituloFR}>Faites votre réservation</Text>
+            </View>
+            <View style={css.inputs}>
+                <Text style={css.label}>Data</Text>
+                <TextInput style={css.campo} keyboardType="numeric" placeholder="dd-mm-aaaa"></TextInput>
+                <Text style={css.label}>Hora</Text>
+                <TextInput style={css.campo} keyboardType="numeric" placeholder="hh.mm"></TextInput>
+                <Text style={css.label}>Pessoas</Text>
+                <TextInput style={css.campo} keyboardType="numeric" placeholder="000"></TextInput>
+                <TouchableOpacity style={css.Botao}>
+                    <Text style={css.btntexto}>Agendar</Text>
+                </TouchableOpacity>
+            </View>
+            
+            <View style={css.calendario}>
+                <CalendarPicker />
+            </View>
         </View>
     );
 }
@@ -28,10 +38,66 @@ const css = StyleSheet.create({
        height: "100%",
        flex: 1,
       alignItems: 'center',
-      justifyContent: 'center',
+
     },
-
-
-
+    calendario: {
+        width: '80%',
+        height: 200
+    },
+    tituloPT: {
+        fontSize: 23,
+        textAlign: 'center',
+        
+    },
+    tituloFR: {
+        fontSize: 15,
+        textAlign: 'center',
+    },
+    linha: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#000",
+        textAlign: 'center',
+        width:"100%",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems:'center',
+        flexDirection: 'row',
+        padding: 10
+    },
+    titulo: {
+        width: '70%',
+        margin: 10,
+        display: 'flex',
+    },
+    Botao: {
+        backgroundColor: "rgba(140, 0, 0, 1)",
+        height: 45,
+        width: '100%',
+        display:"flex",
+        alignItems:"center",
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginTop:20,
+        marginBottom: 10
+    }, 
+    btntexto:{
+        color: "white",
+        fontSize: 20
+    },
+    campo:{
+        fontSize: 20,
+        backgroundColor: '#fff',
+        marginTop: 10,
+        height: 50,
+        width: '100%',
+        borderRadius: 10,
+        padding: 10
+    },
+    label:{
+        fontSize: 20,
+        marginTop: 10
+    },
+    inputs:{
+        width: '65%'
+    },
 })
- //<Calendario></Calendario>
