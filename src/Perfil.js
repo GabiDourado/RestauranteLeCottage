13f58dce4d6e9d816ed8ab3a5e1435f27f391fb8
GabiDourado <gabi.dourados2xd@gmail.com>
2024-04-07 22:52:27 -0300

    ajustes css do perfil

--- src/Perfil.js
+++ src/Perfil.js
@@ -1,48 +1,41 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './Context/UserContext'
 
 export default function Perfil() {
   return (
     <View style={css.Todo}>
-
-<Image style={css.conta} source={{uri: "https://cdn-icons-png.flaticon.com/512/3736/3736502.png",}}/>
-
+      <Image style={css.conta} source={{uri: "https://cdn-icons-png.flaticon.com/512/3736/3736502.png",}}/>
       <View style={css.Dados} >
-      <Text style={css.Dadostxt}>Nome: Fulaninho</Text>
-      <Text style={css.Dadostxt}>Cidade: Pindamonhangaba</Text>
-      <Text style={css.Dadostxt}>CEP: 17280000</Text>
-      <Text style={css.Dadostxt}>Total de pedidos: 150</Text>
-      <Text style={css.Dadostxt}>Prato mais pedido: Parmegiana</Text>
+        <Text style={css.Dadostxt}>Nome: Fulaninho</Text>
+        <Text style={css.Dadostxt}>Cidade: Pindamonhangaba</Text>
+        <Text style={css.Dadostxt}>CEP: 17280000</Text>
+        <Text style={css.Dadostxt}>Total de pedidos: 150</Text>
+        <Text style={css.Dadostxt}>Prato mais pedido: Parmegiana</Text>
       </View >
-
       <TouchableOpacity style={css.Botao}>
             <Text style={css.btntexto}>Mudar de conta</Text>
       </TouchableOpacity>
-
       <TouchableOpacity style={css.Botao}>
             <Text style={css.btntexto}>Sair</Text>
       </TouchableOpacity>
-
-            
-    
-    
     </View>
   )
 }
 
 const css = StyleSheet.create({
   Todo: {
-    backgroundColor: "rgba(250, 224, 226, 1)",
-   width: "100%",
-   height: "100%",
-   flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
+      backgroundColor: "rgba(250, 224, 226, 1)",
+      width: "100%",
+      height: "100%",
+      display:'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
 },
 Dados:{
   backgroundColor:"white",
-  height:300,
-  width:'70%',
+  height:350,
+  width:'80%',
   borderRadius: 10,
   display:"flex",
   alignItems:"center",
@@ -50,7 +43,9 @@ Dados:{
   
 },
 Dadostxt:{
-  marginBottom:20,
+  marginTop:18,
+  marginBottom:18,
+  fontSize: 17
 },
 Botao:{
   backgroundColor: "rgba(140, 0, 0, 1)",
@@ -67,10 +62,7 @@ btntexto:{
   fontSize: 20
 },
 conta: {
-  width: 130,
-  height: 130,
+  width: '50%',
+  height: 200,
 },
-
-
-
 })
